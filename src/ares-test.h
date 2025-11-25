c24fa9852445cc26c3c4682cf4b9c8a63916c656
Valentin Dudouyt <valentin.dudouyt@gmail.com>
2025-11-25 11:46:05 +0200

    Move struct HostEnt to ares-test.h

---
+++ src/ares-test.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <ostream>
+#include <vector>
+#include <netdb.h>
+
+struct HostEnt {
+  HostEnt() : addrtype_(-1)
+  {
+  }
+
+  HostEnt(const struct hostent *hostent);
+  std::string              name_;
+  std::vector<std::string> aliases_;
+  int                      addrtype_;  // AF_INET or AF_INET6
+  std::vector<std::string> addrs_;
+};
+
+std::ostream &operator<<(std::ostream &os, const HostEnt &result);
--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@
 #include <cstddef>
 #include <netdb.h>
 #include "dns-proto.h"
+#include "ares-test.h"
 
 extern "C" {
 #include "impl.h"
@@ -15,66 +16,6 @@ extern "C" {
 
 using ::testing::_;
 using ::testing::Return;
-
-struct HostEnt {
-  HostEnt() : addrtype_(-1)
-  {
-  }
-
-  HostEnt(const struct hostent *hostent);
-  std::string              name_;
-  std::vector<std::string> aliases_;
-  int                      addrtype_;  // AF_INET or AF_INET6
-  std::vector<std::string> addrs_;
-};
-
-HostEnt::HostEnt(const struct hostent *hostent) : addrtype_(-1) {
-  if (!hostent)
-    return;
-
-  if (hostent->h_name)
-    name_ = hostent->h_name;
-
-  if (hostent->h_aliases) {
-    char** palias = hostent->h_aliases;
-    while (*palias != nullptr) {
-      aliases_.push_back(*palias);
-      palias++;
-    }
-  }
-
-  addrtype_ = hostent->h_addrtype;
-
-  if (hostent->h_addr_list) {
-    char** paddr = hostent->h_addr_list;
-    while (*paddr != nullptr) {
-      std::string addr = AddressToString(*paddr, hostent->h_length);
-      addrs_.push_back(addr);
-      paddr++;
-    }
-  }
-}
-
-std::ostream& operator<<(std::ostream& os, const HostEnt& host) {
-  os << "{'";
-  if (host.name_.length() > 0) {
-    os << host.name_;
-  }
-  os << "' aliases=[";
-  for (size_t ii = 0; ii < host.aliases_.size(); ii++) {
-    if (ii > 0) os << ", ";
-    os << host.aliases_[ii];
-  }
-  os << "] ";
-  os << "addrs=[";
-  for (size_t ii = 0; ii < host.addrs_.size(); ii++) {
-    if (ii > 0) os << ", ";
-    os << host.addrs_[ii];
-  }
-  os << "]";
-  os << '}';
-  return os;
-}
 
 class IHelloSayer {
 public:
